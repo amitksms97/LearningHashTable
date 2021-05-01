0bdc6ead5e160e791868ee47b6347dc47d4bb2a0
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:08:43 +0530

    Add project files.

---
+++ LearningHashTable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace LearningHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+            table.Add("title", "MyWebsite");
+
+            var s = (String)table["title"];
+            Console.WriteLine(s);
+        }
+    }
+}
